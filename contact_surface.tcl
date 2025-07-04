56d8a0889b4b020615724ca9072e80e27f326384
jojexcan <58234852+jojexcan@users.noreply.github.com>
2025-07-04 12:09:26 -0300

    Update contact_surface.tcl

--- contact_surface.tcl
+++ contact_surface.tcl
@@ -39,7 +39,7 @@ array set alpha {
 }
 
 # Atom type definitions (polar and nonpolar)
-set def_P  {name "N.*" "O.*" "P.*" "S.*"}
+set def_P  {charge >= 0.2 or charge <= -0.2}
 set def_NP "not ($def_P)"
 
 # Output file
